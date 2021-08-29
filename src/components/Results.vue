736743ca1f144ed49b973577bc8080ee3755ec1a
Marko Markotić <markomarkotic05@gmail.com>
2021-08-29 16:52:04 +0200

    initial commit

---
+++ src/components/Results.vue
@@ -0,0 +1,37 @@
+<template>
+  <p class="score">Reaction time - {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = 'Great Reflexes';
+    } else if (this.score < 400) {
+      this.rank = 'Good Reflexes';
+    } else {
+      this.rank = 'You can do better, try again!';
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  margin-top: 50px;
+  font-size: 2.5em;
+  color: #f12155;
+  font-weight: bold;
+}
+
+.score {
+  font-size: 1.2rem;
+}
+</style>
